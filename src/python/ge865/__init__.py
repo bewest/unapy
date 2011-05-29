170deee13a8e8020bb17c9b888fa20de78943ff1
Ben West <bewest@gmail.com>
2011-05-29 02:57:14 -0700

    hello world

---
+++ src/python/ge865/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def test():
+  logger.info("hello world test")
+  logger.debug("hello world test")
+
+#####
+# EOF
---
+++ src/python/hello.py
@@ -0,0 +1,17 @@
+
+
+import logging
+import sys
+import ge865
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+ge865.logger.setLevel(logging.DEBUG)
+
+
+if __name__ == '__main__':
+  logging.info('hello world')
+  logging.debug('hello world')
+  ge865.test()
+
+#####
+# EOF
