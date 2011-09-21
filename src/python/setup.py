e54169db28fcc61bf0b8059e2e8d7a35315237fb
Ben West <bewest@gmail.com>
2011-09-20 22:41:06 -0700

    add license to setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -8,6 +8,7 @@ setup(
   description      = 'AT 3gpp commands',
   author           = 'Ben West',
   author_email     = 'bewest@gmail.com',
+  license          = 'MIT',
   packages         = ['unapy',],
   install_requires = [ 'pyserial', 'gevent', 'python-messaging'
   ]
